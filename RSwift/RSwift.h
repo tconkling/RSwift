2ebca7c4483e4b04a6db71da1cc9e57fb2e97ade
Tim Conkling <tconkling@gmail.com>
2018-01-18 11:20:04 -0800

    Xcode stub project

---
+++ RSwift/RSwift.h
@@ -0,0 +1,19 @@
+//
+//  RSwift.h
+//  RSwift
+//
+//  Created by Tim Conkling on 1/18/18.
+//  Copyright © 2018 Tim Conkling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSwift.
+FOUNDATION_EXPORT double RSwiftVersionNumber;
+
+//! Project version string for RSwift.
+FOUNDATION_EXPORT const unsigned char RSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSwift/PublicHeader.h>
+
+
